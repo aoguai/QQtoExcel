ab54d7031d8916d7e189b18e7e16fba95e85369f
Abyss <abyss0518@outlook.com>
2022-07-31 12:50:04 +0800

    增加了拖放，重新设计了交互逻辑

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ from Thread import WorkThread
 
 
 class UI(QWidget, Ui_Form):
-    ThreadSignal = pyqtSignal
+    """
+    界面类
+    """
 
     def __init__(self):
         super().__init__()
@@ -28,6 +30,15 @@ class UI(QWidget, Ui_Form):
         self.NameTitle = '昵称'
         self.VIDTitle = 'QQ（邮箱）'
         self.ContentTitle = '内容'
+        self.SourceFilePathInput.installEventFilter(QEventHandler(self))  # 安装事件过滤器，实现拖放
+        self.TargetPathInput.installEventFilter(QEventHandler(self))  # 安装事件过滤器，实现拖放
+        self.customtitlecheckBox.setToolTip("无需更改可留空")
+        self.TargetPathInput.setToolTip("默认或不填为”out“")
+        self.TableNameInput.setToolTip("默认或不填为”Test“")
+        self.SourceFilePathInput.setToolTip("必填")
+        self.SourceFileSelect.setToolTip("文本框支持拖放")
+        self.TargetSelect.setToolTip("文本框支持拖放")
+        self.progressBar.hide()
 
     # 下面为槽函数
     @pyqtSlot()
@@ -46,11 +57,17 @@ class UI(QWidget, Ui_Form):
 
     @pyqtSlot()
     def on_TargetPathInput_editingFinished(self):
-        self.TargetPath = self.TargetPathInput.text()
+        if self.TargetPathInput.text() == '':
+            self.TargetPath = './out'
+        else:
+            self.TargetPath = self.TargetPathInput.text()
 
     @pyqtSlot()
     def on_TableNameInput_editingFinished(self):
-        self.TableName = self.TableNameInput.text()
+        if self.TableNameInput.text() == '':
+            self.TableName = 'Test'
+        else:
+            self.TableName = self.TableNameInput.text()
 
     @pyqtSlot()
     def on_customTime_editingFinished(self):
@@ -99,6 +116,11 @@ class UI(QWidget, Ui_Form):
 
     @pyqtSlot(bool)
     def on_customtitlecheckBox_clicked(self, flag: bool):
+        """
+        自定义按钮槽函数
+        :param flag:
+        :return: None
+        """
         self.label_4.setEnabled(flag)
         self.label_5.setEnabled(flag)
         self.label_6.setEnabled(flag)
@@ -107,6 +129,10 @@ class UI(QWidget, Ui_Form):
         self.customName.setEnabled(flag)
         self.customContent.setEnabled(flag)
         self.customTime.setEnabled(flag)
+        self.customVID.setToolTip("无需更改可留空")
+        self.customTime.setToolTip("无需更改可留空")
+        self.customName.setToolTip("无需更改可留空")
+        self.customContent.setToolTip("无需更改可留空")
         if not flag:
             self.TimeTitle = '时间'
             self.NameTitle = '昵称'
@@ -119,26 +145,35 @@ class UI(QWidget, Ui_Form):
         if progress == 100:
             self.StartButton.setEnabled(True)
             QMessageBox.information(self, '已完成！', '任务已完成！！', QMessageBox.Ok)
-            # os.startfile(self.TargetPath)
+            self.progressBar.hide()
+            self.StartButton.show()
+        # os.startfile(self.TargetPath)
 
     def start_check(self) -> bool:
-        if self.SourceFilePath == '' or os.path.isfile(self.SourceFilePath):
-            # if True:
-            if os.path.isdir(self.TargetPath):
-                if self.TableName == '':
-                    self.TableName = 'Test'
-                return True
-            elif self.TargetPath == './out':
+        """
+        开始检查
+        检查输入是否符合要求
+        :return: 符合返回True，不符合返回False
+        """
+        if self.SourceFilePath == '':
+            QMessageBox.warning(self, '请输入文件地址', '请输入文件地址', QMessageBox.Ok)
+            return False
+        elif not os.path.isfile(self.SourceFilePath):
+            QMessageBox.warning(self, "文件不存在", "文件不存在，请重新输入！！！", QMessageBox.Ok)
+            return False
+        elif self.SourceFilePath[-3:].lower() != 'txt':
+            QMessageBox.warning(self, '文件类型错误', '文件类型错误，程序只接受.TXT格式文件')
+            return False
+        elif not os.path.isdir(self.TargetPath):  # 检查保存文件夹是否已存在
+            if self.TargetPath == './out':
                 os.mkdir('./out')
-                if self.TableName == '':
-                    self.TableName = 'Test'
                 return True
             else:
                 QMessageBox.warning(self, '文件夹不存在', '文件夹不存在，请重新输入！！！')
                 return False
         else:
-            QMessageBox.warning(self, "文件不存在", "文件不存在，请重新输入！！！", QMessageBox.Ok)
-            return False
+            return True
+
 
     @pyqtSlot()
     def on_StartButton_clicked(self):
@@ -152,6 +187,28 @@ class UI(QWidget, Ui_Form):
             self.WorkThread.start()
         else:
             self.StartButton.setEnabled(True)
+            return
+        self.StartButton.hide()
+        self.progressBar.show()
+
+
+class QEventHandler(QtCore.QObject):
+    def eventFilter(self, obj, event):
+        """
+		处理窗体内出现的事件，如果有需要则自行添加if判断语句；
+		目前已经实现将拖到控件上文件的路径设置为控件的显示文本；
+		"""
+        if event.type() == QtCore.QEvent.DragEnter:
+            event.accept()
+        if event.type() == QtCore.QEvent.Drop:
+            md = event.mimeData()
+            if md.hasUrls():
+                # 此处md.urls()的返回值为拖入文件的file路径列表，即支持多文件同时拖入；
+                # 此处默认读取第一个文件的路径进行处理，可按照个人需求进行相应的修改
+                url = md.urls()[0]
+                obj.setText(url.toLocalFile())
+                return True
+        return super().eventFilter(obj, event)
 
 
 if __name__ == '__main__':
